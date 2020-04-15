1520999f98449f0f297e6fc2ba7c920070aab141
Anna Józefowicz <anna.jozefowicz.86@gmail.com>
2020-04-15 18:43:50 +0200

    Updates

---
+++ precache-manifest.13f69d4c378bfc3c9326a6949405fe9b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfa42e142b9623d3f8954a32e6727e3d",
+    "url": "/mypokemons/index.html"
+  },
+  {
+    "revision": "ff1548f2de246ba02b65",
+    "url": "/mypokemons/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "e3a4d1de376cb0c468b7",
+    "url": "/mypokemons/static/css/main.953b1c18.chunk.css"
+  },
+  {
+    "revision": "ff1548f2de246ba02b65",
+    "url": "/mypokemons/static/js/2.460cc6d6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/mypokemons/static/js/2.460cc6d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3a4d1de376cb0c468b7",
+    "url": "/mypokemons/static/js/main.78ee7b68.chunk.js"
+  },
+  {
+    "revision": "198bb839cd0a4b898a5d",
+    "url": "/mypokemons/static/js/runtime-main.1701f0a8.js"
+  }
+]);
